eaa20fcfce32a0f0aef566d5dcdbf515ac1daccd
yogiflogic <yogi_valentinik@yahoo.com>
2021-08-07 06:13:33 +0700

    clean code index

--- index.php
+++ index.php
@@ -84,19 +84,3 @@
     <script src="login-form/js/main.js"></script>
   </body>
 </html>
-
-<?php
-function hitung_umur($tanggal_lahir){
-	$birthDate = new DateTime($tanggal_lahir);
-	$today = new DateTime("today");
-	if ($birthDate > $today) { 
-	    exit("0 tahun 0 bulan 0 hari");
-	}
-	$y = $today->diff($birthDate)->y;
-	$m = $today->diff($birthDate)->m;
-	$d = $today->diff($birthDate)->d;
-	return $y." tahun ".$m." bulan ".$d." hari";
-}
-
-echo hitung_umur("1991-09-01");
-?>
